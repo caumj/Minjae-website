31b40cdc44e5f19e3cf86b5adef16757c70fe5a1
김민재 <caumj@naver.com>
2016-03-20 18:44:44 +0900

    signup  부분 수정함

--- myproject/home/views.py
+++ myproject/home/views.py
@@ -1,4 +1,7 @@
+# -​*- coding: utf-8 -*​-
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -11,7 +14,34 @@ def about(request):
     return render(request, "about.html")
 
 def signup(request):
-    return render(request, "signup.html")
+    error = None
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        password2 = request.POST.get("password2")
+        # Sign up user
+
+        # Password match
+        if password != password2:
+            error = "비밀번호가 일치하지 않습니다."
+
+        # Username exist
+        users = User.objects.filter(username=username)
+        if len(users) > 0:
+            error = "이 아이디로 가입 된 유저가 있습니다."
+
+        # Email exist
+        users = User.objects.filter(email=email)
+        if len(users) > 0:
+            error = "이 이메일로 가입 된 유저가 있습니다."
+
+        if error is None:
+            # Sign up user
+            user = User.objects.create_user(username, email, password)
+            user.save()
+            return redirect('user')
+    return render(request, "signup.html", { "error": error })
 
 def order(request):
     return render(request, "order.html")
