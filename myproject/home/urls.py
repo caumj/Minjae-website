f773b000e2cf0abdb29c31b20068a659213788f5
김민재 <caumj@naver.com>
2016-03-20 17:26:26 +0900

    about, signup, order, user뷰와 템플릿 정리 완료

--- myproject/home/urls.py
+++ myproject/home/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^signin/$', views.signin, name='signin')
+    url(r'^signin/$', views.signin, name='signin'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^order/$', views.order, name='order'),
+    url(r'^user/$', views.user, name='user'),
 ]
--- myproject/home/views.py
+++ myproject/home/views.py
@@ -6,3 +6,15 @@ def index(request):
 
 def signin(request):
     return render(request, "signin.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def signup(request):
+    return render(request, "signup.html")
+
+def order(request):
+    return render(request, "order.html")
+
+def user(request):
+    return render(request, "user.html")
